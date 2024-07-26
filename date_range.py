0c673b0b46f39473650aee43899b5ab853dc04c3
Francis Doran <fcondoran@gmail.com>
2024-07-25 18:54:04 -0700

    optimizations

--- date_range.py
+++ date_range.py
@@ -1,39 +1,8 @@
-""" Get stuff out of Netfile v2 API
-"""
 import pandas as pd
 import datetime as dt
-import copy
-from pprint import PrettyPrinter
-from pathlib import Path
-import os
+import numpy as np
 import requests
-
-BASE_URL = 'https://netfile.com/api/campaign'
-CONTRIBUTION_FORM = 'F460A'
-EXPENDITURE_FORM = 'F460E'
-
-PARAMS = { 'aid': 'COAK' }
-
-def get_auth_from_env_file(filename: str='.env'):
-    """ Split .env file on newline and look for API_KEY and API_SECRET
-        Return their values as a tuple
-    """
-    env_file=Path(filename)
-    auth_keys = [ 'API_KEY', 'API_SECRET' ]
-    if env_file.exists():
-        auth = tuple( v for _, v in sorted([
-            ln.split('=') for ln in
-            env_file.read_text(encoding='utf8').strip().split('\n')
-            if ln.startswith(auth_keys[0]) or ln.startswith(auth_keys[1])
-        ], key=lambda ln: auth_keys.index(ln[0])))
-    else:
-        auth=tuple(os.environ[key] for key in auth_keys)
-            
-    return auth
-
-AUTH=get_auth_from_env_file()
-
-pp = PrettyPrinter()
+from v2api.create_socrata_csv import AUTH, BASE_URL, PARAMS
 
 def get_form(form,offset=0):
     """ Get filings with matching form type
@@ -54,44 +23,37 @@ def get_form(form,offset=0):
         return results, body
     
 class form460:
-    def __init__(self, rpt_date, date_range, filing_id, trash=0) -> None:
+    def __init__(self, rpt_date, date_range, filing_id) -> None:
         self.date_range = date_range
         self.rpt_date = rpt_date
         self.filing_id = filing_id        
-        self.trash = trash
-    def copy(self):
-        return copy.deepcopy(form460(self.rpt_date, self.date_range, self.filing_id))
-    
+    def update(self, new_date_range):
+        self.date_range = new_date_range
+
 def is_iso_str_in_range(iso_str, date_range):
     if iso_str:
         return dt.datetime.fromisoformat(iso_str).date().toordinal() in date_range
     else:
         return True
 
-    
-def date_range_fix(filter_df, filing_date = {}):
-    formlist = sorted([item[11] for item in filter_df.itertuples()], key=lambda x: x.rpt_date)
-    # subsets lose all date range
-    superceded = [base for base in formlist if any(base.date_range.issubset(noble.date_range) and base.filing_id != noble.filing_id and noble.rpt_date > base.rpt_date for noble in formlist)]
-    for base in superceded:
-        base.date_range = set()
-        filing_date[base.filing_id] = base.date_range
-    formlist_copy = [form.copy() for form in formlist if form.date_range]
+# This should cut the complexity from O(n^2) to O(n)
+def date_range_fix(df, result_dict):    
+    array1 = np.array(df['filingNid'])
+    array2 = np.array(df['date_range'].apply(lambda x: x.date_range))
+    i = 0
+    for array in array2[1:]:    
+        if array2[i]:
+            array2[i+1:] = array2[i+1:] - np.repeat(array2[i], len(array2[i+1:]))
+        i+=1
+    result_dict.update(dict(zip(array1, array2)))
+    return result_dict
 
-    for base in formlist_copy:
-        other_date_ranges = [noble.date_range for noble in formlist if noble.rpt_date > base.rpt_date and noble.filing_id != base.filing_id]
-        
-        if other_date_ranges:
-            base.date_range = base.date_range - set.union(*other_date_ranges)
-        filing_date[base.filing_id] = base.date_range
-    return filing_date
-
-fppc460 = get_form('FPPC460')[0]
-fppc465 = get_form('FPPC465')[0]
-contribution_form = fppc460 + fppc465
-# Filter and create the data
-contribution_form_data = [
-    {
+def main():
+    fppc460 = get_form('FPPC460')[0]
+    fppc465 = get_form('FPPC465')[0]
+    contribution_form = fppc460 + fppc465
+    # Filter and create the data
+    contribution_df=pd.DataFrame({
         'filerNid': item['filerMeta']['filerId'],
         'filingNid': item['filingNid'],
         'commonName': item['filerMeta']['commonName'],
@@ -99,19 +61,39 @@ contribution_form_data = [
         'SOS ID': item['filerMeta']['strings'].get('Registration_CA SOS', None),
         'amendmentType': item['filingMeta']['amendmentType'],
         'amendmentSequence': item['filingMeta']['amendmentSequence'],
-        'rpt_date': dt.datetime.strptime(item['filingMeta']['legalFilingDate'], '%Y-%m-%d').date().toordinal(),
         'from_date': dt.datetime.strptime(item['filingMeta']['startDate'], '%Y-%m-%d').date().toordinal(),
         'thru_date': dt.datetime.strptime(item['filingMeta']['endDate'], '%Y-%m-%d').date().toordinal(),
+        'rpt_date': dt.datetime.fromisoformat(item['filingMeta']['legalFilingDateTime']),
+        'date_range': form460
+        (
+            dt.datetime.strptime(item['filingMeta']['legalFilingDate'], '%Y-%m-%d').date().toordinal(),
+            set
+            (
+                range
+                (
+                    dt.datetime.strptime(item['filingMeta']['startDate'], '%Y-%m-%d').date().toordinal(),
+                    dt.datetime.strptime(item['filingMeta']['endDate'], '%Y-%m-%d').date().toordinal()+1
+                )
+            ),
+            item['filingNid'],
+        )
     } 
     for item in contribution_form
-    if item['filingMeta']['legalFilingDate'] and item['filingMeta']['startDate'] and item['filingMeta']['endDate']
-]
-contribution_df=pd.DataFrame(contribution_form_data)
-# Create 'date_range' column
-contribution_df['date_range'] = [form460(row['rpt_date'],set(range(row['from_date'],row['thru_date']+1)),row['filingNid'], index) for index, row in contribution_df.iterrows()]
+    if item['filingMeta']['legalFilingDate'] and item['filingMeta']['startDate'] and item['filingMeta']['endDate'])
 
+    # sort filings by report date, more recent report dates will be prefered this way
+    contribution_df = contribution_df.sort_values('rpt_date', ascending=False)
+    # get indices of the columns which cover the same periods as another by the same filer
+    dupe_indices = contribution_df[contribution_df.duplicated(subset=['filerNid', 'from_date', 'thru_date'], keep='first')].index
+    # empty the date_range but keep the row
+    contribution_df.loc[dupe_indices, 'date_range'] = contribution_df.loc[dupe_indices, 'date_range'].apply(lambda x: x.update(set()) or x)
 
+    filing_date = {}
 
-filing_date = {}
-for id in contribution_df['filerNid'].unique():
-    filing_date = date_range_fix(contribution_df[contribution_df['filerNid']==id],filing_date)
+    for id in contribution_df['filerNid'].unique():
+        filing_date = date_range_fix(contribution_df[contribution_df['filerNid']==id],filing_date)
+
+    return filing_date
+
+if __name__ == "__main__":
+    filing_date = main()
